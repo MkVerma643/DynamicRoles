4f5f18e40ed450beee8982ebb1f0ffccfb4f79bf
Mukesh Verma <mukeshverma643@gmail.com>
2023-02-13 16:38:33 +0530

    Generating the UI

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ namespace Database\Seeders;
 use App\Models\Role;
 use App\Models\Permission;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,10 +19,11 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('admin'),
+        ]);
 
         $adminRole = Role::create(['name' => 'admin']);
         $editorRole = Role::create(['name' => 'editor']);
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PermissionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,6 +28,23 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('roles', [RoleController::class, 'index'])->name('roles.index');
+    Route::post('roles', [RoleController::class, 'store'])->name('roles.store');
+    Route::get('roles/create', [RoleController::class, 'create'])->name('roles.create');
+    Route::get('roles/{item}', [RoleController::class, 'show'])->name('roles.show');
+    Route::put('roles/{item}', [RoleController::class, 'update'])->name('roles.update');
+    Route::delete('roles/{item}', [RoleController::class, 'destroy'])->name('roles.destroy');
+    Route::get('roles/{item}/edit', [RoleController::class, 'edit'])->name('roles.edit');
+
+    Route::get('permissions', [PermissionController::class, 'index'])->name('permissions.index');
+    Route::post('permissions', [PermissionController::class, 'store'])->name('permissions.store');
+    Route::get('permissions/create', [PermissionController::class, 'create'])->name('permissions.create');
+    Route::get('permissions/{item}', [PermissionController::class, 'show'])->name('permissions.show');
+    Route::put('permissions/{item}', [PermissionController::class, 'update'])->name('permissions.update');
+    Route::delete('permissions/{item}', [PermissionController::class, 'destroy'])->name('permissions.destroy');
+    Route::get('permissions/{item}/edit', [PermissionController::class, 'edit'])->name('permissions.edit');
+
 });
 
 require __DIR__.'/auth.php';
